4a2f760d214fa78e7e77b8e75ca2eef6ce197128
Jandy <reverdy@protonmail.com>
2019-11-06 21:13:12 -0800

    changed hamburger button to log out btn. Created new modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import ModalLogOut from '../ModalLogOut'
 
 function Header() {
     return (
@@ -8,14 +9,15 @@ function Header() {
             <nav className="navbar-light custom-hd row">
 
                 <div className="col-6">
-                <img className="header-logo" src="./images/p3-logo3.png" alt="scatter" />
+                    <img className="header-logo" src="./images/p3-logo3.png" alt="scatter" />
                 </div>
 
                 <div className="header-div col-3 test1">
-                <i className="material-icons">person</i>
+                    <i className="material-icons">person</i>
                 </div>
                 <div className="header-div col-3">
-                    <button type="button" className="btn btn-primary btn-sm header-test" > <i className="material-icons">dehaze</i></button>
+                    {/* <button type="button" className="btn btn-primary btn-sm header-test" > <i className="material-icons">dehaze</i></button> */}
+                    <ModalLogOut />
                 </div>
             </nav>
         </header>
