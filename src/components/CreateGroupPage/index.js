831de948adcf0350518618f5c81d10d93fb9b685
Michelle Santiago <msantiago2222@yahoo.com>
2019-11-07 11:03:49 -0800

    axios call on submit event

--- src/components/CreateGroupPage/index.js
+++ src/components/CreateGroupPage/index.js
@@ -6,19 +6,39 @@ import SquareButton from '../SquareButton';
 import Banner from '../Banner'
 import ModalAdd from '../ModalAddPerson';
 import ModalDropPin from '../ModalDropPin';
+import Axios from 'axios';
+import APIURL from '../../utils/APIURL';
 
 class CreateGroupPage extends React.Component {
-    // handleSubmit = submitEvent => {
-    // 	submitEvent.preventDefault();
-    // 	this.props.handleLogOut();
-    // };
+    state = {
+        groupName: "",
+        groupDescription: ""
+    }
 
-    // const handleSubmit = submitEvent => {
-    //     submitEvent.preventDefault();
-    //     // This is for submitting the form
-    // };
+    handleSubmit = submitEvent => {
+        // in order to post data we first need to create object for data to post
+        
+        const groupData = {
+            group_name: this.state.groupName,
+            description: this.state.description
+        }
 
-    // onSubmit={handleSubmit}
+        Axios.post(APIURL("/api/group"), groupData, { withCredentials: true }).then (data=> console.log (data)).catch (error=> console.log(error))
+    };
+
+    handleInputChange = event => {
+        console.log(event.target.name)
+        console.log(event.target.value)
+        const value = event.target.value
+        const name = event.target.name
+        this.setState(
+            {
+                [name]: value
+            }
+        )
+    }
+
+
 
     render() {
         return (
@@ -40,11 +60,11 @@ class CreateGroupPage extends React.Component {
                     <form>
                         <div className="form-group">
                             <label htmlFor="formGroupExampleInput2"></label>
-                            <input type="text" className="form-test1" id="" placeholder="Group name" data-length="20" />
+                            <input onChange={this.handleInputChange} name="groupName" type="text" className="form-test2" id="" placeholder="Group name" data-length="20" />
 
                             <div class="form-group">
                                 <label for="exampleFormControlTextarea1"></label>
-                                <textarea class="form-test2" id="" rows="3" placeholder="Description"></textarea>
+                                <textarea onChange={this.handleInputChange} class="form-test2" id="" rows="3" name="groupDescription" placeholder="Description"></textarea>
                             </div>
 
                             <input type="submit" style={{ position: 'absolute', left: -1000, top: -1000, visibility: 'hidden' }} />
@@ -70,7 +90,7 @@ class CreateGroupPage extends React.Component {
 
                     <div className="bottom-btn-container2">
                         {/* This button should create the group and populate a new button/icon on the group management page */}
-                        <SquareButton type="submit" buttonTitle="DONE" />
+                        <SquareButton type="submit" buttonTitle="DONE" onClick={this.handleSubmit}/>
                     </div>
                 </div>
 
