ac959c0a597b5c8824e1946c35b48c3e7a76c300
Jandy <reverdy@protonmail.com>
2019-11-06 18:36:14 -0800

    quickstart btn and functionality created

---
+++ src/components/QuickStartBtn/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import './style.css'
+
+
+function Quickstart(props) {
+    const handleSubmit = submitEvent => {
+        submitEvent.preventDefault();
+        props.handleQuickstart();
+    };
+    return (
+        <div>
+            <Button className="external-modal-button" onClick={handleSubmit}>Quick Start</Button>
+        </div>
+    )
+}
+
+export default Quickstart;
