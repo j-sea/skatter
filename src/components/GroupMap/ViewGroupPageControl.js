7db6311fa5830c51040c327c09da76841a7c5f75
Jandy <reverdy@protonmail.com>
2019-11-11 19:36:37 -0800

    accepted members now showing

--- src/components/GroupMap/ViewGroupPageControl.js
+++ src/components/GroupMap/ViewGroupPageControl.js
@@ -1,16 +1,16 @@
-import {Control} from 'ol/control';
+import { Control } from 'ol/control';
 
 var ViewGroupPageControl = /*@__PURE__*/(function (Control) {
   function ViewGroupPageControl(opt_options) {
-		console.log(opt_options);
-		var options = opt_options || {};
+    console.log(opt_options);
+    var options = opt_options || {};
 
     var button = document.createElement('button');
     button.innerHTML = '<i class="material-icons">arrow_back</i>';
 
     var element = document.createElement('div');
-		element.className = 'ol-unselectable ol-control';
-		element.id = 'map-view-group-page-control';
+    element.className = 'ol-unselectable ol-control';
+    element.id = 'map-view-group-page-control';
     element.appendChild(button);
 
     Control.call(this, {
@@ -19,12 +19,12 @@ var ViewGroupPageControl = /*@__PURE__*/(function (Control) {
     });
 
     button.addEventListener('click', e => {
-			options.history.push(options.newURL);
-		});
+      options.history.push(options.newURL);
+    });
   }
 
-  if ( Control ) ViewGroupPageControl.__proto__ = Control;
-  ViewGroupPageControl.prototype = Object.create( Control && Control.prototype );
+  if (Control) ViewGroupPageControl.__proto__ = Control;
+  ViewGroupPageControl.prototype = Object.create(Control && Control.prototype);
   ViewGroupPageControl.prototype.constructor = ViewGroupPageControl;
 
   return ViewGroupPageControl;
